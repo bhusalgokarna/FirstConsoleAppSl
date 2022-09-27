0309f92bb83aadceec1e5ae8e92b40e85276b135
Intec <Intec@PC215-06>
2022-09-27 11:45:19 +0200

    Add project files.

---
+++ FirstConsoleApp/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.SetCursorPosition(13, 5);
+            Console.WriteLine("welcome to intec");
+            
+            Console.WriteLine("jouw les begint om 09:00 uur");
+            Console.WriteLine("En om 16:00 uurs ben je met je let gedaan");
+            Console.WriteLine("druk op iets om verder te gaan");
+            Console.ReadLine();
+            Console.WriteLine("Je neemt pauze om 12:20 tot 12:50");
+            Console.WriteLine("Hier hebben we geen lunchzaal");
+            Console.ReadLine();
+
+            Console.Clear();
+
+            Console.ReadLine();
+
+        }
+    }
+}
